fdd59a7485b08ff6a1f3d12b97b953e0eaad4709
Sanjay Pais <sdpais@hotmail.com>
2024-03-02 20:33:38 -0500

    Add project files.

---
+++ Test.WebAPIWDapper/Program.cs
@@ -0,0 +1,44 @@
+using  WebAPIDapper.Services;
+using Dapper;
+using Prometheus;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Dapper
+DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDbService, DbService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//adding metrics related to HTTP
+app.UseHttpMetrics(options =>
+{
+    options.AddCustomLabel("host", context => context.Request.Host.Host);
+});
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//adding the Metric Endpoint
+app.UseMetricServer();
+
+app.Run();
